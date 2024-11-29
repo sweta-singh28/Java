0568ff7abd8955bd4ac378de1e4a54c4bd364949
sweta-singh28 <swetasinghgs28@gmail.com>
2024-11-29 23:45:36 +0530

    java

---
+++ ConditionalStatements.java
@@ -0,0 +1,18 @@
+class ConditionalStatements {
+    public static void main(String[] args) {
+        //conditional statements
+        boolean  isSunUp = true;
+        if(isSunUp == true)
+            System.out.println("day");
+        else
+            System.out.println("night");    
+            
+            
+       int age = 30;
+       if(age>18)
+           System.out.println("can vote");     
+       else
+           System.out.println("Cannot vote");
+    }
+    
+}
---
+++ Loops.java
@@ -0,0 +1,18 @@
+class Loops {
+    public static void main(String[] args){
+        //loops
+        //print 1 to 100
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+        
+        System.out.println("Printing using while loop");
+
+        int i=1;
+        while(i<=100){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
